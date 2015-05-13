bc2c2e317749b9faa29f187d182712bbe70d97b2
Robert Pessagno <robert.pessagno@rosetta.com>
2015-05-13 10:17:15 -0700

    Media query mixin, example usage

--- src/index.php
+++ src/index.php
@@ -1,29 +1,33 @@
 <?php include_once('inc/header.php'); ?>
 
-<h1>Heading 1</h1>
-
-<h2>Heading 2</h2>
-
-<h3>Heading 3</h3>
-
-<h4>Heading 4</h4>
-
-<h5>Heading 5</h5>
-
-<h6>Heading 6</h6>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse perferendis temporibus quos alias est facere placeat, saepe nobis qui repellendus, animi nesciunt quod consequatur quisquam omnis laudantium distinctio ullam neque!</p>
-
-<ul>
-    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus assumenda placeat atque, fugit numquam nisi magnam perferendis nemo labore ipsam, quidem, libero vitae. Iure saepe esse libero nam tempora accusamus.</li>
-    <li>Nesciunt qui, sapiente fugit magni accusantium, eveniet consequatur, dolore harum odio maiores expedita neque rem reiciendis. Sed dicta quidem esse iste fugit minus omnis libero eum dolorem, deleniti impedit quia!</li>
-    <li>Fuga, vitae nihil accusamus molestias delectus ut iusto incidunt iste, tempora esse, facere facilis dolorum ab natus aut suscipit fugiat cupiditate ipsum nemo enim veritatis saepe quis excepturi eligendi. Eos.</li>
-</ul>
-
-<ol>
-    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi modi accusamus delectus illum possimus, iusto voluptas. Ducimus illum non dolore reprehenderit? Adipisci consequatur unde fuga sequi quaerat, nemo autem porro.</li>
-    <li>Sunt laudantium, inventore. Labore sunt facere ab tenetur adipisci, unde. Consequatur dolorum est expedita adipisci atque sint labore dolor, cumque ratione unde doloribus aliquid pariatur fugit, ex eos harum incidunt.</li>
-    <li>Quia quis magnam iste, quisquam itaque, possimus voluptate distinctio, ab cumque, sunt at! Reprehenderit facere harum inventore, illum earum minima doloremque voluptates ipsam ipsa nam? Excepturi odio cupiditate quia aut!</li>
-</ol>
+<div class="wrapper">
+  
+  <h1>Heading 1</h1>
+  
+  <h2>Heading 2</h2>
+  
+  <h3>Heading 3</h3>
+  
+  <h4>Heading 4</h4>
+  
+  <h5>Heading 5</h5>
+  
+  <h6>Heading 6</h6>
+  
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse perferendis temporibus quos alias est facere placeat, saepe nobis qui repellendus, animi nesciunt quod consequatur quisquam omnis laudantium distinctio ullam neque!</p>
+  
+  <ul>
+      <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus assumenda placeat atque, fugit numquam nisi magnam perferendis nemo labore ipsam, quidem, libero vitae. Iure saepe esse libero nam tempora accusamus.</li>
+      <li>Nesciunt qui, sapiente fugit magni accusantium, eveniet consequatur, dolore harum odio maiores expedita neque rem reiciendis. Sed dicta quidem esse iste fugit minus omnis libero eum dolorem, deleniti impedit quia!</li>
+      <li>Fuga, vitae nihil accusamus molestias delectus ut iusto incidunt iste, tempora esse, facere facilis dolorum ab natus aut suscipit fugiat cupiditate ipsum nemo enim veritatis saepe quis excepturi eligendi. Eos.</li>
+  </ul>
+  
+  <ol>
+      <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi modi accusamus delectus illum possimus, iusto voluptas. Ducimus illum non dolore reprehenderit? Adipisci consequatur unde fuga sequi quaerat, nemo autem porro.</li>
+      <li>Sunt laudantium, inventore. Labore sunt facere ab tenetur adipisci, unde. Consequatur dolorum est expedita adipisci atque sint labore dolor, cumque ratione unde doloribus aliquid pariatur fugit, ex eos harum incidunt.</li>
+      <li>Quia quis magnam iste, quisquam itaque, possimus voluptate distinctio, ab cumque, sunt at! Reprehenderit facere harum inventore, illum earum minima doloremque voluptates ipsam ipsa nam? Excepturi odio cupiditate quia aut!</li>
+  </ol>
+  
+</div>
 
 <?php include_once('inc/footer.php'); ?>
