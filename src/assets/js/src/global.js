daf593550c8ded66cec10be7f5f7ba79cc04e2c1
Robert Pessagno <robert.pessagno@rosetta.com>
2015-09-29 17:13:33 -0700

    fit bids, fast click, global.js, placeholders, print

---
+++ src/assets/js/src/global.js
@@ -0,0 +1,10 @@
+(function (window, document, $, undefined) {
+  'use strict';
+  // place entire program inside of this closure
+
+FastClick.attach(document.body);
+
+//$('.thing-with-videos').fitVids();
+
+// End Document Ready
+})(window, document, jQuery);
