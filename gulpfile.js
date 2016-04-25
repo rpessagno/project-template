5731be4e71c1d8198b1cf2e1158cd499a5fa2181
Robert Pessagno <robert.pessagno@rosetta.com>
2016-04-25 10:08:28 -0700

    copyfiles, removed gulp-sync,
    - copyfiles moves everything except js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,42 @@
 //----------------------------------------
 // Plugins
 //----------------------------------------
-var gulp = require('gulp'),
-  sass          = require('gulp-sass'),
-  autoprefixer  = require('gulp-autoprefixer'),
-  cssnano       = require('gulp-cssnano'),
-  jshint        = require('gulp-jshint'),
-  stylish       = require('jshint-stylish'),
-  uglify        = require('gulp-uglify'),
-  imagemin      = require('gulp-imagemin'),
-  concat        = require('gulp-concat'),
-  changed       = require('gulp-changed'),
-  livereload    = require('gulp-livereload'),
-  del           = require('del');
-  gulpsync      = require('gulp-sync')(gulp);
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    cssnano       = require('gulp-cssnano'),
+    jshint        = require('gulp-jshint'),
+    stylish       = require('jshint-stylish'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    concat        = require('gulp-concat'),
+    changed       = require('gulp-changed'),
+    livereload    = require('gulp-livereload'),
+    del           = require('del');
+
+
+//----------------------------------------
+// Clean
+//----------------------------------------
+gulp.task('clean', function() {
+    return del(['target/*']);
+});
+
+//----------------------------------------
+// Copy files
+//----------------------------------------
+gulp.task('copyfiles', function () {
+  gulp.src([
+    './src/**/*',
+    '!./src/assets/scss',
+    '!./src/assets/scss/**/*',
+    '!./src/assets/js/**/*',
+    '!./src/assets/images/**/*'
+    ])
+    .pipe(changed('target'))
+    .pipe(gulp.dest('target'))
+    .pipe(livereload());
+});
 
 //----------------------------------------
 // CSS
@@ -62,7 +85,6 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('target/assets/js'));
 });
 
-
 //----------------------------------------
 // Images
 //----------------------------------------
@@ -73,44 +95,33 @@ gulp.task('images', function () {
 });
 
 //----------------------------------------
-// Copy files
-//----------------------------------------
-gulp.task('copyfiles', function () {
-  gulp.src('./src/**/*.{html,php,txt,xml}')
-    .pipe(changed('target'))
-    .pipe(gulp.dest('target'))
-    .pipe(livereload());
-});
-
-//----------------------------------------
-// Clean
-//----------------------------------------
-gulp.task('clean', function (cb) {
-  del(['target/*'], cb);
-});
-
-//----------------------------------------
 // Watch
 //----------------------------------------
 gulp.task('watch', function () {
   livereload.listen();
+  gulp.watch([
+    './src/**/*',
+    '!./src/assets/scss',
+    '!./src/assets/scss/**/*',
+    '!./src/assets/js/**/*',
+    '!./src/assets/images/**/*'
+    ], ['copyfiles']);
   gulp.watch('src/assets/scss/**/*.scss', ['styles']);
   gulp.watch('src/assets/js/**/*.js', ['scripts']);
-  gulp.watch('./src/**/*.{html,php,txt}', ['copyfiles']);
 });
 
 //----------------------------------------
 // Default Task
 //----------------------------------------
-gulp.task('default', gulpsync.sync(['styles', 'scripts', 'copyfiles', 'images'], ['clean']));
+
+gulp.task('default', ['clean'], function() {
+  gulp.run(['copyfiles', 'styles', 'scripts', 'images'])
+});
 
 //----------------------------------------
 // Watch
 //----------------------------------------
-gulp.task('dev', gulpsync.sync(['styles', 'scripts', 'copyfiles', 'images', 'watch'], ['clean']));
 
-// To Do
-/*
-- No linting on JS libs
-- Jade
-*/
+gulp.task('dev', ['clean'], function() {
+  gulp.run(['copyfiles', 'styles', 'scripts', 'images', 'watch'])
+});
