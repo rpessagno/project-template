0a4a7f23def84375b68b15b58f9df193e26cbb9e
Robert Pessagno <rpessagno@gmail.com>
2022-09-12 11:12:02 -0700

    gulp 4.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -2,78 +2,66 @@
 // Gulpfile
 //========================================
 
+const { src, dest, watch, series } = require('gulp');
 
 //----------------------------------------
 // Plugins
 //----------------------------------------
 
-var gulp          = require('gulp');
-var sass          = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
-var cssnano       = require('gulp-cssnano');
-var jshint        = require('gulp-jshint');
-var stylish       = require('jshint-stylish');
-var uglify        = require('gulp-uglify');
-var concat        = require('gulp-concat');
-var browserSync   = require('browser-sync').create();
-
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer  = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const browsersync = require('browser-sync').create();
 
 //----------------------------------------
 // Variables
 //----------------------------------------
 
-var domainName = 'project-template';
-var theme      = 'project-template';
+var domainName = 'projecttemplate';
+var themeFolder = './target/wp-content/themes/project-template/';
 
 //----------------------------------------
 // CSS
 //----------------------------------------
 
-gulp.task('styles', function () {
-  return gulp.src('src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+function styles() {
+  return src('src/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cssnano({
       zindex: false
     }))
-    .pipe(gulp.dest('target/wp-content/themes/' + theme + '/'))
-    .pipe(browserSync.stream());
-});
-
+    .pipe(dest(themeFolder, { sourcemaps: '.' } ));
+}
 
 //----------------------------------------
 // JS
 //----------------------------------------
 
-// Concat
-gulp.task('scripts', function () {
-  return gulp.src([
-    './src/js/lib/jquery.js',
-    './src/js/lib/*.js',
-    './src/js/src/global.js',
-    './src/js/src/*.js'
-  ])
+function jsConcat() {
+  return src([ './src/js/lib/jquery.js', './src/js/lib/*.js', './src/js/src/global.js', './src/js/src/*.js' ], { sourcemaps: true })
     .pipe(concat('main.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('target/wp-content/themes/' + theme + '/assets/js'))
-    .pipe(browserSync.stream());
-});
+    .pipe(dest(themeFolder + 'assets/js', { sourcemaps: '.' } ));
+}
 
-// Lint
-gulp.task('scripts-lint', function () {
-  return gulp.src([
-    './src/js/src/*.js',
-  ])
+function jsLint() {
+  return src('./src/js/src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
-});
+}
+
 
 //----------------------------------------
-// Watch
+// Browsersync
 //----------------------------------------
 
-gulp.task('watch', function() {
-  browserSync.init({
+function browsersyncServe(cb) {
+  browsersync.init({
     // MAMP
     proxy: 'local.' + domainName + '.com'
     // No MAMP
@@ -81,22 +69,32 @@ gulp.task('watch', function() {
     //   baseDir: 'target'
     // }
   });
-  gulp.watch('./target/wp-content/themes/' + theme + '/**/*')
-    .on('change', browserSync.reload);
-  gulp.watch('src/scss/**/*.scss', gulp.parallel('styles'));
-  gulp.watch('src/js/**/*.js', gulp.parallel('scripts', 'scripts-lint'));
-});
+  cb();
+}
 
-
-//----------------------------------------
-// Default Task
-//----------------------------------------
-
-gulp.task('default', gulp.parallel('styles', 'scripts', 'scripts-lint'));
-
+function browersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
 
 //----------------------------------------
-// Dev Task
+// Watch
 //----------------------------------------
 
-gulp.task('dev', gulp.parallel('styles', 'scripts', 'scripts-lint', 'watch'));
+function watchTask() {
+  watch(themeFolder + '**/*.php', browersyncReload);
+  watch('src/scss/**/*.scss', series(styles, browersyncReload));
+  watch('src/js/**/*.js', series(jsConcat, jsLint, browersyncReload));
+}
+
+//----------------------------------------
+// Tasks
+//----------------------------------------
+
+exports.default = series(
+  styles,
+  jsConcat,
+  jsLint,
+  browsersyncServe,
+  watchTask
+);
