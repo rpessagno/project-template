abd9e934216d4e046bb5d2d84c58294da7e05eb4
Robert Pessagno <rpessagno@gmail.com>
2023-05-12 12:46:48 -0700

    favicon png

--- target/wp-content/themes/project-template/header.php
+++ target/wp-content/themes/project-template/header.php
@@ -7,8 +7,8 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 
 <!-- Icons -->
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/images/global/favicon.ico" type="image/x-icon">
-<link rel="icon" href="<?php bloginfo('template_directory'); ?>/assets/images/global/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/images/global/favicon.png" type="image/x-icon">
+<link rel="icon" href="<?php bloginfo('template_directory'); ?>/assets/images/global/favicon.png" type="image/x-icon">
 <link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/assets/images/global/touch-icon.png">
 
 <!-- App -->
@@ -17,6 +17,7 @@
 <meta name="application-name" content="App Short Name" />
 <meta name="apple-mobile-web-app-title" content="App Full Name">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="theme-color" content="#000" />
 
 <?php wp_head(); ?>
 
